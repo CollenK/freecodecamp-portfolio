25d8d2aee730b1ef9c7b3e050d679eac958eee40
CollenK <collen.kriel@gmail.com>
2016-11-23 16:51:31 +0700

    Add Scrollspy & smooth scroll

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$('body').scrollspy({ target: '.navbar' })
+
+
+$(document).ready(function() {
+// Smooth scroll
+var $root = $('html, body');
+	
+$('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function() {
+        window.location.hash = href;
+    });
+      
+    return false;
+});
+
+// Remove focus from button on click
+$(".btn").mouseup(function() {
+    $(this).blur();
+});
+
+// END DOCUMENT READY
+});
